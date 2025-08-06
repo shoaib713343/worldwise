f5466d748c0f2bf53508f6c8f86a55c77bb563d3
Shoaib <shoaib713343@gmail.com>
2025-08-06 18:15:38 +0530

    first commit

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,10 @@
+import styles from "./AppNav.module.css" 
+function AppNav() {
+    return (
+        <div className={styles.nav}>
+            Appnav
+        </div>
+    )
+}
+
+export default AppNav
